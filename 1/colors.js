d1e926617949dcf9bc4d69227fdc7d309447f035
dayoung-lee <55811738+dayoung-lee@users.noreply.github.com>
2020-01-21 18:25:58 -0500

    Add files via upload

--- 1/colors.js
+++ 1/colors.js
@@ -1,20 +1,27 @@
 var Body = {
     setColor:function (color){
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        // jQuery
+        $('body').css('color', color);
     },
     setBackgroundColor:function (color){
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        // jQuery
+        $('body').css('backgroundColor', color);
     }
 }
 var Links = {
     setColor:function (color){
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while(i < alist.length){
-            console.log(alist[i]);
-            alist[i].style.color = color;
-            i++;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //     console.log(alist[i]);
+        //     alist[i].style.color = color;
+        //     i++;
+        // }
+        //
+        // jQuery
+        $('a').css('color', color);
     }
 }
 function nightDayHandler(self){
